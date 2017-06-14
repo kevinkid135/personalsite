75c3815b71cc702a1dbfcf97a289bc8f474e06f1
kevinkid135 <kevinkid228@hotmail.com>
2017-06-14 15:42:49 -0400

    Added ElizaClone and adjusted more placeholder text

--- src/index.js
+++ src/index.js
@@ -27,11 +27,11 @@ function Cover() {
         <header>
             <div id="info">
                 <h1 className="display-1 animated bounceInLeft">Kevin Chan</h1>
-                <h2 className="animated bounceInRight">Software Developer/Web Developer</h2>
-                <h3 className="animated bounceInLeft">Software Developer/Web Developer</h3>
-                <h4 className="animated bounceInRight">Software Developer/Web Developer</h4>
-                <h5 className="animated bounceInLeft">Software Developer/Web Developer</h5>
-                <h6 className="animated bounceInRight">Software Developer/Web Developer</h6>
+                <h2 className="animated bounceInRight">Software Developer</h2>
+                <h3 className="animated bounceInLeft">Web Developer</h3>
+                <h4 className="animated bounceInRight">Problem Solver</h4>
+                <h5 className="animated bounceInLeft">Friend</h5>
+                <h6 className="animated bounceInRight">Status: Looking for an Internship</h6>
             </div>
         </header>
     )
@@ -45,17 +45,32 @@ function ProjectSection() {
                 <Project
                     title="Personal Website"
                     pic="http://bit.ly/1Rs9q0y"
-                    desc={<p>You're looking at it!<p>Responsive design for all devices</p></p>}
+                    desc={<p>You're looking at it!<p>Responsive design for all devices.</p></p>}
                     link="https://github.com/kevinkid135" // TODO
                     tags={['HTML5', 'CSS3', 'Bootstrap4', 'React.js', 'animate.css']}
                 />
                 <Project
                     title="League of Legends Chest Checker"
                     pic="http://bit.ly/1Rs9q0y"
-                    desc={<p>LoLCC calculates 'First Win of the Day, in addition to displaying game data through Riot Games' API</p>}
+                    desc={<p>LoLCC calculates 'First Win of the Day', in addition to displaying game data through Riot Games' API.</p>}
                     link="http://lolcc.me"
                     git="https://github.com/kevinkid135/LoLChestChecker"
-                    tags={['HTML5', 'CSS3', 'Bootstrap3', 'jQuery', 'API']}
+                    tags={['HTML5', 'CSS3', 'JavaScript', 'Bootstrap3', 'jQuery', 'API']}
+                />
+                <Project
+                    title="Eliza Clone"
+                    pic="http://bit.ly/1Rs9q0y"
+                    desc={<p>Eliza mimics texting Rogerian psychotherapist by using AI methodologies such as pattern matching and substitution.</p>}
+                    link="https://cdn.rawgit.com/kevinkid135/ElizaClone/d7981ea7/index.html"
+                    git="https://github.com/kevinkid135/ElizaClone"
+                    tags={['HTML5', 'CSS3', 'JavaScript', 'jQuery', 'AI']}
+                />
+                <Project
+                    title="Facebook VR"
+                    pic="http://bit.ly/1Rs9q0y"
+                    desc={<p>Facebook VR is a protoype of how we may use virtual reality as a social media platform.</p>}
+                    git="https://github.com/victormimo/CISC325"
+                    tags={['Unity', 'C#']}
                 />
             </div>
         </div>
